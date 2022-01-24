b6cddcbe52e0d90b5475d703c82df3c49d8a2a32
Vykula <viktoria.zheneral@gmail.com>
2022-01-24 18:45:31 +0200

    just ' to "

--- js/delay.js
+++ js/delay.js
@@ -9,15 +9,15 @@ $(window).resize(function () {
 var delay = 0;
 
 function inViewport() {
-    $('.to-show-delay').each(function () {
+    $(".to-show-delay").each(function () {
         var div = $(this).offset().top;
         var bottom = $(window).scrollTop();
 
         var animDelay = " animate__delay-" + delay + "s";
 
         if (div < (bottom + $(window).height())) {
-            $(this).removeClass('to-show-delay hidden');
-            $(this).addClass('visible animate__animated animate__fadeIn' + animDelay);
+            $(this).removeClass("to-show-delay hidden");
+            $(this).addClass("visible animate__animated animate__fadeIn" + animDelay);
             delay++;
         }
     });
