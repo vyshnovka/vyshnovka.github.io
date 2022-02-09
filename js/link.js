752cf8f95389aaca6dc9ad8f5f085241e738e7d8
Vykula <viktoria.zheneral@gmail.com>
2022-02-09 22:00:48 +0200

    function link a bit optimized

--- js/link.js
+++ js/link.js
@@ -1,8 +1,3 @@
-function goLink(url, newTab) {
-	if (newTab) {
-	 	window.open(url, '_blank', 'height=250, width=650');
-	} 
-	else {
-		window.open(url, '_blank');
-	}
+function goLink(url) {
+	window.open(url, '_blank');
 };
