13ee688a51ba73809d7ae87e6d8b0053ee7708e8
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2022-02-18 23:30:34 +0200

    adaptive menu test build

--- js/menu.js
+++ js/menu.js
@@ -6,15 +6,21 @@ function menuItem(item) {
 function showPage(element) {
     $(".container").hide();
     $("#" + element).show();
+
+    $(".top").css("background-color", "#d1e5eb");
+    $(".hamburger").removeClass("is-active");
+    $(".hide-for-mobile").hide();
 }
 
 function displayMenu() {
     if ($(".hamburger").hasClass("is-active")) {
+        $(".top").css("background-color", "#d1e5eb");
         $(".hamburger").removeClass("is-active");
         $(".menu").hide();
 
     }
     else {
+        $(".top").css("background-color", "#2b2b35");
         $(".hamburger").addClass("is-active");
         $(".menu").show();
     }
