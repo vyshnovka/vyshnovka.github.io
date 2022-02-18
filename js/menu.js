0c573e48ae41744e1fb04e2f66a68e1cbe557c10
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2022-02-18 16:11:06 +0200

    hamburger menu added

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,12 @@ function menuItem(item) {
     $(".menu>p.menu-item-chosen").removeClass("menu-item-chosen");
     $(item).addClass("menu-item-chosen");
 }
+
+function displayMenu() {
+    if ($(".hamburger").hasClass("is-active")) {
+        $(".hamburger").removeClass("is-active");
+    }
+    else {
+        $(".hamburger").addClass("is-active");
+    }
+}
