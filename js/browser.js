b75920c2c8994c7386e769a8bb868e74b4e8f77e
Viktoria Zheneral <viktoria.zheneral@gmail.com>
2022-02-18 17:52:38 +0200

    trying to fool Safari with letter-spacing rendering

---
+++ js/browser.js
@@ -0,0 +1,15 @@
+function browserDetect() {
+    let userAgent = navigator.userAgent;
+
+    if (userAgent.match(/chrome|chromium|crios/i)) {
+        browserName = "chrome";
+    } else if (userAgent.match(/firefox|fxios/i)) {
+        browserName = "firefox";
+    } else if (userAgent.match(/safari/i)) {
+        $(".big-text").style.letterSpacing = "-2px";
+    } else if (userAgent.match(/opr\//i)) {
+        browserName = "opera";
+    } else if (userAgent.match(/edg/i)) {
+        browserName = "edge";
+    }
+}
