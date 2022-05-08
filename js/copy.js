7835e07f7b638653937d6e0779a193350eebd93e
Vykula <viktoria.zheneral@gmail.com>
2022-05-09 01:13:08 +0300

    adjusted for left part changes only

--- js/copy.js
+++ js/copy.js
@@ -8,15 +8,19 @@ function copyEmail() {
     switch (countClick) {
         case 1:
             document.getElementById('copy-email').innerHTML = "Email copied!"
+            document.getElementById('copy-email').style.cursor = "pointer";
             break;
         case 2:
             document.getElementById('copy-email').innerHTML = "Email copied again. Not sure why you did this."
+            document.getElementById('copy-email').style.cursor = "pointer";
             break;
         case 3:
             document.getElementById('copy-email').innerHTML = "Trust me, email's already been copied. Stop."
+            document.getElementById('copy-email').style.cursor = "pointer";
             break;
         case 4:
             document.getElementById('copy-email').innerHTML = "Why are you so persistent?.."
+            document.getElementById('copy-email').style.cursor = "pointer";
             break;
         case 50:
             document.getElementById('copy-email').innerHTML = "Congratulations, you've wasted a few minutes of your life!"
@@ -25,6 +29,7 @@ function copyEmail() {
             break;
         default:
             document.getElementById('copy-email').innerHTML = "Okay, fine, <b>vyshnovka@gmail.com</b>."
+            document.getElementById('copy-email').style.cursor = "pointer";
             break;
     }
 }
